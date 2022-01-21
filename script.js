ae1857cd081b746097b0c3292ae36b1316080ea1
JC <22549889@sun.ac.za>
2022-01-21 05:41:47 +0200

    GUI changes

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function computerPlay(){
     }else
         return "SCICCORS";
 }
-
 function winner(){
     buttons.forEach((button)=>{
         button.disabled=true;
@@ -17,23 +16,41 @@ function winner(){
         moreinfo.textContent=`You win!`;
     else
         moreinfo.textContent=`You lose`;
+    restart.style="display: inline;"
+    const i=document.createElement('i');
+    computer.innerHTML="";
+    i.classList.add("fas", "fa-hand-middle-finger");
+    computer.appendChild(i);
 }
 
 
 function newGame(){
+    restart.style="display: none;"
     buttons.forEach((button)=>{
         button.disabled=false;
     })
     restart.disabled=true;
     counter=loseCounter=round=0;
     score.textContent=`Your score: ${counter} | Computer score: ${loseCounter} `;
-    moreinfo.textContent=`Lets go`;
-    log.innerHTML="";
+    moreinfo.textContent=`Lets go pussy!`;
+    log.textContent="";
 }
 
 function playRound(human,e){
+
+    computer.innerHTML="";
+    const i=document.createElement('i');
+    computer.appendChild(i);
     round++;
     let pc=computerPlay();
+    if(pc=="SCICCORS")
+        i.classList.add("fas","fa-hand-scissors");
+    else if(pc=="ROCK")
+        i.classList.add("fas","fa-hand-rock");
+    else
+        i.classList.add("fas","fa-hand-paper");
+        
+ 
     if(pc==human){
         moreinfo.textContent=`${human} equals ${pc}`;
     }else if(human=="ROCK"&&pc=="PAPER"||human=="PAPER"&&pc=="SCICCORS"||human=="SCICCORS"&&pc=="ROCK"){
@@ -43,9 +60,10 @@ function playRound(human,e){
         moreinfo.textContent=`${human} beats ${pc}`;
         counter++;
     } 
-    score.textContent=`Your score: ${counter} | Computer score: ${loseCounter} | Round: ${round}`;
+    score.textContent=`Your score: ${counter} | Computer score: ${loseCounter}`;
+    pround.textContent=`Round: ${round}`
     const p=document.createElement('p');
-    p.innerHTML=moreinfo.textContent;
+    p.textContent=`Round ${round}: ${moreinfo.textContent}`;
     log.appendChild(p);
     if(loseCounter>=5||counter>=5)
         winner();
@@ -56,8 +74,11 @@ let round=0;
 const buttons=document.querySelectorAll('.game');
 const score=document.querySelector('.score')
 const restart=document.querySelector('.restart');
+const pround=document.querySelector('.round');
 const log=document.querySelector('.log');
+const computer=document.querySelector('.pc');
 restart.disabled=true;
+restart.style="display: none;"
 const moreinfo=document.querySelector('.moreinfo')
 buttons.forEach((button)=>{
     button.addEventListener('click',(e)=>
